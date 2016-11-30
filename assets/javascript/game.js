0d36e061d5b3ac44a227377c75d36da12ae66f59
Matt White <mattwhite1337@gmail.com>
2016-11-30 18:21:51 -0500

    Changed to only create new random letter if you guess it

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,21 +2,25 @@ var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
 
-var cpu = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var cpu = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o"
+			,"p","q","r","s","t","u","v","w","x","y","z"];
+
+function cpuGuess() {
+	return cpu[Math.floor(Math.random() * cpu.length)];
+}
+
+var newCpuGuess = cpuGuess();
 
 
 document.onkeyup = function(event) {
 	
-	function cpuGuess() {
-		return cpu[Math.floor(Math.random() * cpu.length)];
-	}
-
-	var newCpuGuess = cpuGuess();
+	console.log(newCpuGuess);
 
 	var keystroke = event.key.toLowerCase();
 
 	if (keystroke === newCpuGuess) {
 		wins++;
+		newCpuGuess = cpuGuess();
 	} else {
 		losses++;
 		guessesLeft--;
